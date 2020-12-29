fb601025bcec2306a9fd0f52ae4ad804fd9a0391
Roman Verekh <vrbdsp@gmail.com>
2020-12-29 15:50:51 +0200

    add css style

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,13 +6,22 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class Controller {
 
     @FXML
     private Button buttonSend;
+
+    @FXML
+    private TextField textMessage;
 
     @FXML
     private FlowPane flowChat;
@@ -30,16 +39,30 @@ public class Controller {
 //        area.setBorder(new Border(new BorderStroke(Color.BLACK,
 //                BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
         //Setting number of pages
-        area.setText("String.valueOf(scrollMes.getVvalue())");
-        area.setStyle("-fx-control-inner-background: pink;" +
-                "-fx-font-size: 16");
+        area.setText(textMessage.getText());
+        //area.setId("left-area");
+        area.getStyleClass().add("left-area");
+//        area.setStyle("-fx-control-inner-background: pink;" +
+//                "-fx-font-size: 13");
 
 
         //area.setStyle("-fx-background-radius: 15");
         //area.setStyle("-fx-border-radius: 15");
         //area.setPrefColumnCount(15);
-        area.setPrefHeight(50);
+        area.setPrefHeight(30.0 * textMessage.getText().length()/25);
         area.setPrefWidth(240);
+
+        FileInputStream input = null;
+        try {
+            input = new FileInputStream("src/img/admin-user-icon-48.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image = new Image(input);
+        ImageView imageView = new ImageView(image);
+        //imageView.getStyleClass().add("left-image");
+        //imageView.setStyle("-fx-padding: 15px;" + "-fx-border-insets: 15px;" + "-fx-background-insets: 15px;");
+        flowChat.getChildren().add(imageView);
 
         flowChat.getChildren().add(area);
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("My Chat");
-        primaryStage.setScene(new Scene(root));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("css/style.css");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
